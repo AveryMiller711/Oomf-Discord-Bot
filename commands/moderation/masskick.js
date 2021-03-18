393a3ba427a7e16603be7452e011915e92e0f9b1
Avery Miller <ajmiller2060@yahoo.com>
2021-03-18 11:45:47 -0400

    fixed masskick command test

--- commands/moderation/masskick.js
+++ commands/moderation/masskick.js
@@ -9,7 +9,7 @@ module.exports = {
         if(!message.member.hasPermission('KICK_MEMBERS')){
             return message.reply(`You do not have permission to use \`masskick\``);
         }
-        if (args.size) {
+        if (args.length) {
             const quantity = args;
             message.channel.send(`You wanted to kick: ${args} people`);
         } else {
