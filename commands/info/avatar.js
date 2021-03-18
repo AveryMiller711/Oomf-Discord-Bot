ac165246fde74d44896489d545874fb8c15cb1e6
Avery Miller <ajmiller2060@yahoo.com>
2021-03-17 23:10:23 -0400

    Revert "Figuring out JSON saving"
    This reverts commit 0ccc7c3b4c98239c43865b920a337018c7f0fb60.

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-var fs = require('fs');
 
 module.exports = {
     name: 'avatar',
@@ -8,25 +7,7 @@ module.exports = {
     description: 'Sends the user\'s avatar.',
     cooldown: 5,
     execute(message, args){
-        try{
-            if(fs.existsSync('./input.json')){
-                var userArray = require('./input.json');
-            }
-        } catch(err) {
-            console.error(err);
-            var userArrayFile = fs.openSync('./input.json', 'a');
-            var userArray = [];
-        }
         const user = message.mentions.users.first() || message.author;
-        //var userArray = [], obj = {name: user.id, friends: [user.tag, user.name]};
-        var obj = {name: user.id, friends: [user.tag, user.name]};
-        userArray.push(obj);
-        fs.writeFile ("./commands/info/input.json", JSON.stringify(userArray), function(err){
-            if(err) throw err;
-            console.log('complete');
-            console.log(userArray[0].name);
-            }
-        );
         const embed = new Discord.MessageEmbed()
             .setTitle('**Avatar for ' + user.tag + '**')
             .setImage(user.displayAvatarURL({ dynamic: true, size: 1024}))
